c8003b90ae42f61922b5777232d395cc1dae130a
Vaibhav-Dubey <vaibhavdubey1999@gmail.com>
2021-05-11 22:14:45 +0530

    added 'add to database' functionality to notifier

--- CovidDocs/C_Docs/views.py
+++ CovidDocs/C_Docs/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Notification
 
 # Create your views here.
 
 def Notifier(request):
+    return render(request,'Notifier.html')
+
+
+def notifier_filled(request):
+    print("notifer_filled")
+    email= request.POST["email"]
+    state=request.POST["state"]
+    district=request.POST["district"]
+    pincode=request.POST["pin"]
+
+    person_info = Notification(email=email,state=state,district=district,pincode=pincode)
+
+    person_info.save()
+
+
     return render(request,'Notifier.html')
 
 def index(request):
