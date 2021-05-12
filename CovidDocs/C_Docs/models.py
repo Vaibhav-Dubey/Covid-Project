b835e779eacd4484b8f2369ee644ef5e418ce091
Vaibhav-Dubey <vaibhavdubey1999@gmail.com>
2021-05-12 14:20:03 +0530

    added database feature

--- CovidDocs/C_Docs/models.py
+++ CovidDocs/C_Docs/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Notification(models.Model):
+    email=models.CharField(max_length=200)
+    state=models.CharField(max_length=20)
+    district=models.CharField(max_length=20)
+    pincode=models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.email
--- CovidDocs/CovidDocs/settings.py
+++ CovidDocs/CovidDocs/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'C_Docs.apps.CDocsConfig',
+    'C_Docs',
 ]
 
 MIDDLEWARE = [
